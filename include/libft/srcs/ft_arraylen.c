ebd616e869010b4641c40ab32cdeec3a3253bb2e
Xavier Chouinard <xchouina@c1r1p3.42quebec.com>
2022-07-26 13:49:11 -0400

    13:49

--- include/libft/srcs/ft_arraylen.c
+++ include/libft/srcs/ft_arraylen.c
@@ -1,21 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_arrayadd.c                                      :+:      :+:    :+:   */
+/*   ft_arraylen.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xchouina <xchouina@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/26 12:34:26 by xchouina          #+#    #+#             */
-/*   Updated: 2022/07/26 13:14:03 by xchouina         ###   ########.fr       */
+/*   Created: 2022/07/26 13:07:50 by xchouina          #+#    #+#             */
+/*   Updated: 2022/07/26 13:23:03 by xchouina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
 
-int	ft_arrayadd(char **tab, char *str_2_add)
+int	ft_arraylen(char **tab)
 {
-	char	**new;
-	int		i;
-	
-		
+	int	i;
+	int	len;
+
+	i = -1;
+	len = 0;
+	while (tab[++i] != NULL)
+		len++;
+	return (len);
 }
