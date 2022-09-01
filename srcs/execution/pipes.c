ac53a5748c44c0613815791da79f9475bd737fd1
Marie soleil Jarry <mjarry@c1r1p2.42quebec.com>
2022-09-01 15:05:39 -0400

    to merge.. again

--- srcs/execution/pipes.c
+++ srcs/execution/pipes.c
@@ -3,8 +3,7 @@
 // NOTE: CREATES PIPES
 // TOKNOW: - if redirection to file before end of cmd line, redirection overwrites dup2 to pipe and pipe is empty. Next cmd will execute with empty imput.
 
-//srch_inputfile(tokens, "<"); -> opens token->next, returns fd of opened file -> returns -1 if inexistant -> return 0 if no redirection
+//srch_inputfile(tokens, "<"); -> srchs while != '|', opens token->next, returns fd of opened file -> returns -1 if inexistant -> return 0 if no redirection
 //srch_outputfile(token, ">", ">>"); -> opens token->next, returns fd of opened file -> returns 1 if no redirection
 
-//
 
