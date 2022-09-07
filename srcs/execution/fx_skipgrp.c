99716eb3b1acf60ec84754b81066ee36d51e7635
Xavier Chouinard <xchouina@c1r1p3.42quebec.com>
2022-09-07 11:42:34 -0400

    added function (skip_group)

---
+++ srcs/execution/fx_skipgrp.c
@@ -0,0 +1,11 @@
+#include "../../include/minishell.h"
+
+t_token	*skip_group(t_token *current_token)
+{
+	int	t;
+
+	t = current_token->group_num;
+	while (current_token->group_num == t)
+		current_token = current_token->next;
+	return (current_token);
+}
