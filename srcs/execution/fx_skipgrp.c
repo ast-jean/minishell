0b908110061569d9a6e42e57be871b8e0a472488
Xavier Chouinard <xchouina@c1r1p3.42quebec.com>
2022-09-07 11:56:16 -0400

    updated fx_skipgrp

--- srcs/execution/fx_skipgrp.c
+++ srcs/execution/fx_skipgrp.c
@@ -5,7 +5,7 @@ t_token	*skip_group(t_token *current_token)
 	int	t;
 
 	t = current_token->group_num;
-	while (current_token->group_num == t)
+	while (current_token->next != NULL && current_token->group_num == t)
 		current_token = current_token->next;
 	return (current_token);
 }
