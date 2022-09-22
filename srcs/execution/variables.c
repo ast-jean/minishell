8e7e9b19488bd4365302fc940e8db3946a50c543
Adam St-jean lejeune <ast-jean@c3r7p2.42quebec.com>
2022-09-22 15:33:52 -0400

    WIP  ' '

--- srcs/execution/variables.c
+++ srcs/execution/variables.c
@@ -42,31 +42,31 @@ char *find_var_inline(char *line)
 		return (NULL);
 }
 
-char	*find_variable(char **env, char *varname)
-{
-	int		i;
-	int		j;
-	int		k;
-	char	*var_value;
-
-	var_value = NULL;
-	i = -1;
-	k = 0;
-	while (env[++i])
-	{
-		j = 0;
-		while (env[i][j] && env[i][j] == varname[j])
-			j++;
-		if (env[i][j] == '=')
-		{
-			j = (int)ft_strlen(varname) + 1;
-			while(env[i][j])
-				var_value = ft_addchar(var_value, env[i][j++], &var_value[k++]);
-			return (var_value);
-		}
-	}
-	return (var_value);
-}
+// char	*find_variable(char **env, char *varname) //REPLACE BY getenv(var_name)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		k;
+// 	char	*var_value;
+//
+// 	var_value = NULL;
+// 	i = -1;
+// 	k = 0;
+// 	while (env[++i])
+// 	{
+// 		j = 0;
+// 		while (env[i][j] && env[i][j] == varname[j])
+// 			j++;
+// 		if (env[i][j] == '=')
+// 		{
+// 			j = (int)ft_strlen(varname) + 1;
+// 			while(env[i][j])
+// 				var_value = ft_addchar(var_value, env[i][j++], &var_value[k++]);
+// 			return (var_value);
+// 		}
+// 	}
+// 	return (var_value);
+// }
 
 char	*delete_var_name(int pos2 ,char *newline)
 {
@@ -128,13 +128,14 @@ char *check_var(char *line, t_vars *vars)
 
 	i = 0;	
 	newline = line;
-
+	(void)vars;
 		while (find_var_inline(newline))
 		{
 			printf("\033[43mdebug: \033[0mi =%d\033[43m-\033[0m\n", i++);
 			var_name = save_varname(find_var_inline(newline)+ 1);
 			printf("\033[43mdebug: \033[0mvar_name =%s\033[43m-\033[0m\n", var_name);
-			var_value = find_variable(vars->env, var_name);
+			// var_value = find_variable(vars->env, var_name);
+			var_value = getenv(var_name);
 			printf("\033[43mdebug: \033[0mvar_value =%s\033[43m-\033[0m\n", var_value);
 			newline = add_varcontent(newline, var_name, var_value);
 			printf("\033[43mdebug: \033[0mnewline =%s\033[43m-\033[0m\n", newline);
