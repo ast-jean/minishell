d55b080d1225970c48addb733add8e19ddb5368c
Adam St-jean lejeune <ast-jean@c3r7p2.42quebec.com>
2022-09-27 09:56:25 -0400

    readding find_variable()

--- srcs/execution/variables.c
+++ srcs/execution/variables.c
@@ -95,6 +95,36 @@ char	*add_varcontent(char *line, char *var_name, char *var_value)
 	return (newline);
 }
 
+
+
+
+char	*find_variable(char **env, char *varname)
+{
+	int i;
+	int j;
+	char *var_value;
+
+	var_value = NULL;
+	i = -1;
+	while (env[++i])
+	{
+		j = 0;
+		while (env[i][j] && env[i][j] == varname[j])
+			j++;
+		if (env[i][j] == '=')
+		{
+			j = (int)ft_strlen(varname) + 1;
+			while(env[i][j])
+			{	
+				var_value = ft_addchar(var_value, env[i][j], &var_value[j]);
+				j++;
+			}
+			return (var_value);
+		}
+	}
+	return (var_value);
+}
+
 char	*check_var(char *line)
 {
 	char	*var_name;
