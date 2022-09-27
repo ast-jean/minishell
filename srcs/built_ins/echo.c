69252f031b7cad039052d61c27a225ed04d16be9
Xavier Chouinard <xchouina@c3r8p5.42quebec.com>
2022-09-27 12:11:30 -0400

    fixed echo until further investigations

--- srcs/built_ins/echo.c
+++ srcs/built_ins/echo.c
@@ -3,9 +3,11 @@
 int	is_n(char *str)
 {
 	int	i;
-	
+
 	i = 1;
-	while (str[i] != '\n')
+	if (str[0] != '-')
+		return (0);
+	while (str[i])
 	{
 		if (str[i] != 'n')
 			return (0);
@@ -14,28 +16,9 @@ int	is_n(char *str)
 	return (1);
 }
 
-char *rm_quotes_echo(char *old)
-{
-	char	*new;
-	int		i;
-	int		j;
-
-	i = -1;
-	j = 0;
-	new = malloc(sizeof(char*) * ft_strlen(old));
-	while (old[++i] != '\n')
-	{
-		if (old[i] != '\"' || old[i] != '\'')
-			i++;
-		else
-			new[j++] = old[i];
-	}
-	return (new);
-}
-
 int	builtin_echo(t_vars *vars)
 {
-	t_token *token;
+	t_token	*token;
 	bool	n;
 
 	n = false;
@@ -45,21 +28,16 @@ int	builtin_echo(t_vars *vars)
 		n = true;
 		token = token->next;
 	}
-	// dprintf(2, "TOKEN CONT = %s\n", token->cont)
-	while (token && ft_strcmp(token->cont, "|") != 0 )
+	while (token && ft_strcmp(token->cont, "|") != 0)
 	{
-		// printf("\033[46m check_var-> %s\033[46m\n", check_var(token->cont, vars));
-		// ft_putstr_fd(token->cont, 1);
 		ft_putstr_fd(remove_quotes(check_var(token->cont)), 1);
-		if (token->next != NULL) 
+		if (token->next != NULL)
 			write(1, " ", 1);
 		token = token->next;
-	// dprintf(2, "im here\n");
 	}
-	if (n == false)
-		dprintf(2, "eeeeeee");
+	if (n == true)
+		return (0);
+	else
 		ft_putstr_fd("\n", 1);
-	// else
-		// return (0);
 	return (0);
 }
